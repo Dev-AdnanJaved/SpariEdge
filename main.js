f34a8998eaad4060f9e8d9a9f6c66fe0cbae9bc4
Adnan Ali <adnanjaved711@gmail.com>
2025-08-25 18:00:46 +0500

    Slider Unavailable

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ function loadHTML(containerId, file) {
 
 loadHTML("header", "files/header.html")    
 loadHTML("hero-section", "files/hero-section.html")    
+loadHTML("second-section", "files/second-section.html")    
+// loadHTML("slider-section", "files/slider-section.html")   
+loadHTML("pricing-section", "files/pricing-section.html")   
+loadHTML("FAQ", "files/FAQ.html")   
+loadHTML("footer", "files/footer.html")   
+
+
+
+
+
